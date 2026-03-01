d2e8eee2ec27429f374bf29eebb5356a7f9fb522
Bita Yeganeh <b.yeganeh85@gmail.com>
2026-03-01 21:20:11 +0200

    wip

--- src/data/projects.js
+++ src/data/projects.js
@@ -11,27 +11,28 @@ const projects = [
   },
   {
     id: 2,
+    title: "WordPress/PHP Project - Tech News",
+    image: "/images/technews.png",
+    alt: "WordPress/PHP Project - Tech News",
+    github: "https://github.com/BitaYeganeh/WordPress-PHP-Project---Tech-News",
+  },
+  
+  {
+    id: 3,
+    title: "JavaScript Project - Pancake Ordering Site",
+    image: "/images/project2.png",
+    alt: "Pancake Order Project",
+    github: "https://github.com/BitaYeganeh/Summer-tasks/tree/main/Pannukakku",
+  },
+  {
+    id: 4,
     title: "Figma Project - Café Website UI ",
     image: "/images/figma-cafe.png",
     alt: "Café website UI design in Figma",
     live: "https://www.figma.com/proto/LOFz2qhMrQFBrMrCsotQfl/prototype-for-a-caf%C3%A9-website?node-id=1-2&starting-point-node-id=1%3A2&t=taVksUnCMCTiGYTj-1",
   },
   {
-    id: 3,
-    title: "Figma - Personal Mood Board",
-    image: "/images/moodboard.png",
-    alt: "Mood Board UI design in Figma",
-    live: "https://www.figma.com/design/T0DHRgheR5owxqJPjMSCew/Mood-Borad?t=0w1cZSds6tZsp8H0-1",
-  },
-  {
-    id: 4,
-    title: "WordPress/PHP Project - Tech News",
-    image: "/images/technews.png",
-    alt: "WordPress/PHP Project - Tech News",
-    github: "https://github.com/BitaYeganeh/WordPress-PHP-Project---Tech-News",
-  },
-  {
-    id: 4,
+    id: 5,
     title: "Wordpress Project - Business College Helsinki",
     image: "/images/bcd.png",
     alt: "Wordpress Project - Business College Helsinki Networking Page",
@@ -39,7 +40,7 @@ const projects = [
       "https://github.com/BitaYeganeh/wordpress_Ohjelmistokehitysprojekti-1/tree/main",
   },
   {
-    id: 5,
+    id: 6,
     title: "Wordpress Project - ABC OF MEDIA Website",
     image: "/images/abc.png",
     alt: "Wordpress Project - ABC OF MEDIA",
@@ -47,7 +48,7 @@ const projects = [
   },
 
   {
-    id: 6,
+    id: 7,
     title: "JavaScript Project - Eläintarha",
     image: "/images/project1.png",
     alt: "Eläintarha JavaScript Project",
@@ -55,21 +56,22 @@ const projects = [
       "https://github.com/BitaYeganeh/HTML_School/tree/main/JS/07-viikko/Eläintarha",
   },
   {
-    id: 7,
-    title: "JavaScript Project - Pancake Ordering Site",
-    image: "/images/project2.png",
-    alt: "Pancake Order Project",
-    github: "https://github.com/BitaYeganeh/Summer-tasks/tree/main/Pannukakku",
+    id: 8,
+    title: "Figma - Personal Mood Board",
+    image: "/images/moodboard.png",
+    alt: "Mood Board UI design in Figma",
+    live: "https://www.figma.com/design/T0DHRgheR5owxqJPjMSCew/Mood-Borad?t=0w1cZSds6tZsp8H0-1",
+
   },
   {
-    id: 8,
+    id: 9,
     title: "JavaScript Project - Pancake kitchen view",
     image: "/images/project3.png",
     alt: "Pancake Project 2",
     github: "https://github.com/BitaYeganeh/Summer-tasks/tree/main/Pannukakku",
   },
   {
-    id: 9,
+    id: 10,
     title: "Flexbox Layout Project",
     image: "/images/project4.png",
     alt: "Flexbox Layout Project",
@@ -77,7 +79,7 @@ const projects = [
       "https://github.com/BitaYeganeh/HTML_School/tree/main/HTML-KURSSI/06-viikko/01_flex-projekti",
   },
   {
-    id: 10,
+    id: 11,
     title: "Flexbox Project",
     image: "/images/project5.png",
     alt: "Flexbox Project 2",
@@ -85,7 +87,7 @@ const projects = [
       "https://github.com/BitaYeganeh/HTML_School/tree/main/HTML-KURSSI/06-viikko/01_flex-projekti",
   },
   {
-    id: 11,
+    id: 12,
     title: "Flexbox Project",
     image: "/images/project6.png",
     alt: "Flexbox Project 3",
@@ -93,7 +95,7 @@ const projects = [
       "https://github.com/BitaYeganeh/HTML_School/tree/main/HTML-KURSSI/06-viikko/01_flex-projekti",
   },
   {
-    id: 12,
+    id: 13,
     title: "Social-links-profile-card",
     image: "/images/project7.png",
     alt: "Social-links-profile-card Project",
@@ -101,7 +103,7 @@ const projects = [
       "https://github.com/BitaYeganeh/Summer-tasks/tree/main/FrontendMentor/Social-links-profile",
   },
   {
-    id: 13,
+    id: 14,
     title: "Base Apparel Coming Soon",
     image: "/images/project8.png",
     alt: "Base Apparel Coming Soon Project",
@@ -109,7 +111,7 @@ const projects = [
       "https://github.com/BitaYeganeh/Summer-tasks/tree/main/FrontendMentor/base-apparel-coming-soon-master",
   },
   {
-    id: 14,
+    id: 15,
     title: "SignUp Form",
     image: "/images/project9.png",
     alt: "SignUp Form Project",
